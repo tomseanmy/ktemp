806a78247690b2b1090955c6a6180e7c98f2c18a
tomsean <tom_sean@outlook.com>
2025-11-24 15:47:41 +0800

    feat(types): 添加LocalDateTime序列化支持
    - 新增LDT类型别名用于LocalDateTime序列化
    - 实现LocalDateTimeSerializer自定义序列化器
    - 支持LocalDateTime与字符串之间的相互转换
    - 解决kotlinx serialization无法直接序列化LocalDateTime的问题
    - 保持与前端交互时的时间格式一致性

--- src/main/kotlin/cn/ts/types/Id.kt
+++ src/main/kotlin/cn/ts/types/Id.kt
@@ -1,10 +1,39 @@
 package cn.ts.types
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.LongAsStringSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
 
 /**
  * Id类型
+ * 在后端使用long类型作为ID的类型，但前端long类型会丢失精度，所以序列化时需要转为String类型
  * @author tomsean
  */
 typealias Id = @Serializable(with = LongAsStringSerializer::class) Long
+
+/**
+ * LocalDateTime类型
+ * 在后端使用LocalDateTime类型，但kotlinx serialization无法序列化LocalDateTime类型，所以需要自定义序列化器
+ */
+typealias LDT = @Serializable(with = LocalDateTimeSerializer::class) LocalDateTime
+
+/**
+ * LocalDateTime序列化器
+ */
+class LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LocalDateTimeString", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        return LocalDateTime.parse(decoder.decodeString())
+    }
+}

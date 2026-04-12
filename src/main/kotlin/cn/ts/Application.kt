f629fcbd32e63bfdf6d4f9b1e3ac3204aae38bf7
tomsean <tom_sean@outlook.com>
2026-04-12 21:02:10 +0800

    refactor(config): 移除未使用的序列化配置导入
    - 删除了 configureSerialization 的导入声明
    - 清理了不再需要的序列化配置依赖

--- src/main/kotlin/cn/ts/Application.kt
+++ src/main/kotlin/cn/ts/Application.kt
@@ -8,7 +8,6 @@ import cn.ts.configure.configureMonitoring
 import cn.ts.configure.configureRedisson
 import cn.ts.configure.configureRouting
 import cn.ts.configure.configureSecurity
-import cn.ts.configure.configureSerialization
 import cn.ts.configure.configureSockets
 import io.ktor.server.application.*
 import io.ktor.server.netty.EngineMain
